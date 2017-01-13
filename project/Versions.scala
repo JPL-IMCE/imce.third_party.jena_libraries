6cf96ec7c8db9483aec490720d70f6358befd1c6
Nicolas F Rouquette <nicolas.f.rouquette@jpl.nasa.gov>
2017-01-13 05:08:50 +0000

    Updated versions & dependencies

--- project/Versions.scala
+++ project/Versions.scala
@@ -2,13 +2,13 @@
 object Versions {
 
   // this project's version
-  val version = "3.4.0"
+  val version = "3.5.0"
   val scala_version = "2.11.8"
 
   // jenaLibs
 
   // http://mvnrepository.com/artifact/org.apache.jena/jena-tdb
-  val jenaTDB = "3.1.0"
+  val jenaTDB = "3.1.1"
 
   // http://mvnrepository.com/artifact/org.apache.jena/jena-fuseki-server
   val jenaFuseki = "2.4.1"
